1b6286c98ffb6e95351ef377f9ab65cd1c995846
smc4718 <smc4718@naver.com>
2025-01-21 02:34:01 +0900

    paris 폴더구조 및 초기설정

---
+++ paris/src/main/java/com/pyj/paris/ParisApplication.java
@@ -0,0 +1,13 @@
+package com.pyj.paris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParisApplication.class, args);
+	}
+
+}
---
+++ paris/src/test/java/com/pyj/paris/ParisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pyj.paris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
